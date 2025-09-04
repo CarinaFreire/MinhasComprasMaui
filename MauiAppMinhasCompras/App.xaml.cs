73243ece7b3cf2fd98b8a5df68f4973fa74548dd
carina <carinafreire.m@gmail.com>
2025-09-04 20:43:31 -0300

    Primeiro commit do projeto MAUI

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        static SQLiteDataBaseHelper _db;
+        public static SQLiteDataBaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.ApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDataBaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
+        public App ()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
